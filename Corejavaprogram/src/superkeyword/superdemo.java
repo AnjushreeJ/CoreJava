eb545b724d25e895b1b88afbc4ccafcba006d67b
Anjushree J <anjushreej5@gmail.com>
2025-10-24 19:44:19 +0530

    Commit

---
+++ Corejavaprogram/src/superkeyword/superdemo.java
@@ -0,0 +1,18 @@
+package superkeyword;
+
+//demo for super keyword
+//variables
+//method
+//constructor
+public class superdemo {
+	
+	int a=60;// variable
+	
+	void drinking() {//method
+		System.out.println("tea");
+	}
+	 superdemo(){ // constructor 
+		 System.out.println("welcome to java");
+	 }
+
+}
