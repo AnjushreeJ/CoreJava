b017bb6152b6c9bb3dd816e53caeeeaaf2572d3b
Anjushree J <anjushreej5@gmail.com>
2025-10-27 22:21:59 +0530

    Commit

---
+++ Corejavaprogram/src/staticprogram/staticblockdemo.java
@@ -0,0 +1,13 @@
+package staticprogram;
+
+//demo for static block
+
+public class staticblockdemo {
+	static {
+		System.out.println("welcome to vkit");
+	}
+	public static void main(String[] args) {
+		System.out.println("welcome to java");
+	}
+
+}
