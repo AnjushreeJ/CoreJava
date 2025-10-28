1710e263ca19309480c670ced5e35abe0c0a66a5
Anjushree J <anjushreej5@gmail.com>
2025-10-28 18:45:19 +0530

    Commit
    Added a demo class for the final keyword in Java.

---
+++ Corejavaprogram/src/finalkeyworddemo/finaldemo.java
@@ -0,0 +1,18 @@
+package finalkeyworddemo;
+
+//demo for final keyword
+//variable
+//method
+//class
+
+public class finaldemo {
+	
+	static int a=8;  // u cant change value
+	
+	static {
+		a=9;
+	}
+	void display() { // cant override
+		System.out.println("welcome to java");
+	}
+}
